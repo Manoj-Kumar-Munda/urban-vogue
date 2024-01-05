fa814d059177a6e05726a1f1137f66b7e90bbf6c
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-01-05 09:28:31 +0530

    added VideoBg component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,39 +1,16 @@
-import React from 'react';
-import bgVideo from '../assets/bg-video.mp4';
-import bgMobile from "../assets/top_banner.webp";
-import bgThumbnail from "../assets/main-bg-thumbnail.webp"
-import '../styles/mainPage.css';
+import Hero from "./Hero";
+import VideoBg from "./VideoBg";
+
 
 const Body = () => {
   return (
-    <div className="w-screen h-screen relative overflow-hidden ">
-      <video
-        className="w-full h-full hidden md:block"
-        poster={bgThumbnail}
-        autoPlay
-        loop
-        muted
-      >
-        <source src={bgVideo} />
-      </video>
-      <div className="w-full h-full md:hidden">
-        <img
-          src={bgMobile}
-          alt={"bg-mobile-banner"}
-          className="object-cover object-center w-full "
-        />
-      </div>
-
-      <div className="hidden  absolute bottom-32 left-0 right-0 md:flex flex-col items-center">
-        <h1 className="text-white font-Josefin-Sans font-bold text-7xl leading-normal">
-          The Veronicas
-        </h1>
-        <h3 className="hover:cursor-pointer font-Josefin-Slab text-white underline underlin underline-offset-2">
-          + EXPLORE THE COLLECTIONS
-        </h3>
-      </div>
-    </div>
+    <>
+      <Hero />
+      <VideoBg />
+      
+      
+    </>
   );
-}
+};
 
-export default Body
+export default Body;
---
+++ src/components/VideoBg.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import videoBg from "../assets/video-bg.mp4"
+import bgThumbnail from "../assets/video_bg_thumbnail.webp"
+
+const VideoBg = () => {
+  return (
+    <div className='w-screen h-screen'>
+        <video playsInline  className='w-full h-full' poster={bgThumbnail} autoPlay loop muted>
+            <source src={videoBg} type="" />
+        </video>
+    </div>
+  )
+}
+
+export default VideoBg
