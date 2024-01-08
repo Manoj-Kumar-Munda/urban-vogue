e177564136c78f464db0db7c602ff0fe2654dd14
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-01-08 09:51:43 +0530

    style fixes

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -5,16 +5,16 @@ import pic3 from "../assets/IMAGE-3.webp";
 
 const Collection = () => {
   return (
-    <section className="px-6">
-      <div className="py-12 ">
+    <section className="px-4 md:px-6">
+      <div className="py-8 md:py-12 ">
         <h1 className="text-center font-Josefin-Slab font-bold text-5xl">
           Other Collections
         </h1>
       </div>
 
       <div className="flex justify-center items-center">
-        <div className="flex justify-center items-center gap-6 overflow-x-auto">
-          <div className="aspect-[2/3] w-full h-full">
+        <div className="relative flex justify-start items-center gap-6 overflow-auto px-2 scroll-smooth">
+          <div className="aspect-[2/3] w-full h-full scroll-card ">
             <img
               src={pic1}
               alt="pic-1"
@@ -25,7 +25,7 @@ const Collection = () => {
             </figcaption>
           </div>
 
-          <div className="aspect-[2/3] w-full h-full">
+          <div className="aspect-[2/3] w-full h-full scroll-card">
             <img
               src={pic3}
               alt="pic-3"
@@ -33,7 +33,7 @@ const Collection = () => {
             />
             <p className="py-2 font-Josefin-Sans">Wild Romance</p>
           </div>
-          <div className=" aspect-[2/3] w-full h-full">
+          <div className=" aspect-[2/3] w-full h-full scroll-card">
             <img
               src={pic2}
               alt="pic-2"
